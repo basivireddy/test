bcb0234b4607f8f8d0b3a638302d0c86cec03256
Basivireddy <d.basivireddy@gmail.com>
2022-11-15 15:01:31 +0530

    Update autoincrement.sh

--- .github/workflows/actions/helm-version-auto-increment/autoincrement.sh
+++ .github/workflows/actions/helm-version-auto-increment/autoincrement.sh
@@ -11,6 +11,7 @@ if [[ -z "${CHART_PATH}" ]]; then
    exit 1
 fi
 
+echo "inputs: helm version: ${HELM_VERSION}, chart_path:  ${CHART_PATH}"
 
 tag=$(git describe --tags --abbrev=0 HEAD --always)
 echo "tag : $tag"
@@ -18,14 +19,13 @@ echo "tag : $tag"
 
 cf=$(git diff --quiet HEAD ${tag} -- ${CHART_PATH})
 
-echo "${cf}"
+echo "cf: $cf"
 
-if ( cf != 0 ) {
+if [[ $cf != 0 ]]; then
    echo "There is a change in helm chart"
-}
-else {
+else 
   echo "There is no change in helm chart"
-}
+fi
 
                   
                   
