8d6943891f253a77d6ad80aeaecf5ace6c285362
Basivireddy <d.basivireddy@gmail.com>
2022-11-15 15:46:28 +0530

    Update autoincrement.sh

--- .github/workflows/actions/helm-version-auto-increment/autoincrement.sh
+++ .github/workflows/actions/helm-version-auto-increment/autoincrement.sh
@@ -13,7 +13,7 @@ fi
 
 echo "inputs: helm version: ${HELM_VERSION}, chart_path:  ${CHART_PATH}"
 
-tag=$(git describe --tags --abbrev=0 HEAD)
+tag=$(git describe --tags --abbrev=0)
 echo "tag : $tag"
 
 
