edacb5ac1fc931bf55e826901dd516199776d035
Basivireddy <d.basivireddy@gmail.com>
2022-11-15 14:56:41 +0530

    Update autoincrement.sh

--- .github/workflows/actions/helm-version-auto-increment/autoincrement.sh
+++ .github/workflows/actions/helm-version-auto-increment/autoincrement.sh
@@ -21,13 +21,10 @@ cf=$(git diff --quiet HEAD ${tag} -- ${CHART_PATH})
 echo "${cf}"
 
 if ( cf != 0 ) {
-                     echo "There is a change in helm chart"
-#                     Updated_HELM_VERSION = sh (script: "echo ${HELM_VERSION} | awk -F. '{\$NF = \$NF + 1;} 1' | sed 's/ /./g'", returnStdout: true).trim()
-#                     echo "${Updated_HELM_VERSION}"
+   echo "There is a change in helm chart"
 }
 else {
   echo "There is no change in helm chart"
-  
 }
 
                   
