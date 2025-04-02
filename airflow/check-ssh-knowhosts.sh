253e637ee3a7881a9a94d4b25e5625e9d4facfac
Basivireddy <d.basivireddy@gmail.com>
2025-04-02 16:37:17 +0530

    Update check-ssh-knowhosts.sh

--- airflow/check-ssh-knowhosts.sh
+++ airflow/check-ssh-knowhosts.sh
@@ -3,15 +3,9 @@
 # Check if the ~/.ssh directory exists
 if [ -d "$HOME/.ssh" ]; then
     echo "SSH directory exists: $HOME/.ssh"
-else
-    echo "SSH directory does NOT exist!"
-    exit 1
-fi
-
-# Check if the known_hosts file exists
-if [ -f "$HOME/.ssh/known_hosts" ]; then
+elif [ -f "$HOME/.ssh/known_hosts" ]; then
     echo "SSH known_hosts file exists: $HOME/.ssh/known_hosts"
 else
-    echo "SSH known_hosts file does NOT exist!"
-    exit 2
+    echo "create"
 fi
+
