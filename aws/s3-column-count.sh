65b7ce7eb5bbed34c9517b404215c354e72d802c
Basivireddy <d.basivireddy@gmail.com>
2025-05-09 08:25:33 +0530

    Update s3-column-count.sh

--- aws/s3-column-count.sh
+++ aws/s3-column-count.sh
@@ -12,3 +12,20 @@ column_count=$(echo "$header" | tr -cd ',' | wc -c)
 total_columns=$((column_count + 1))
 
 echo "Total number of columns: $total_columns"
+
+
+#!/bin/bash
+
+# Path to your local CSV file
+FILE="yourfile.csv"
+
+# Read the first line (header)
+header=$(head -n 1 "$FILE")
+
+# Count commas
+column_count=$(echo "$header" | tr -cd ',' | wc -c)
+
+# Total columns = commas + 1
+total_columns=$((column_count + 1))
+
+echo "Total number of columns: $total_columns"
